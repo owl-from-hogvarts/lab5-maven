71e68c89c0dedef3bc0b8736e3010d0e0fc7aadb
owl-from-hogvarts <transftank@mail.ru>
2023-04-26 14:03:57 +0300

    chore: reorganize imports

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/collection/OrganisationDataSinkSourceFactory.java
+++ organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/collection/OrganisationDataSinkSourceFactory.java
@@ -1,12 +1,10 @@
 package net.whitehorizont.apps.organization_collection_manager.core.collection;
 
-import net.whitehorizont.apps.organization_collection_manager.core.collection.OrganisationElement.Builder;
-
 public class OrganisationDataSinkSourceFactory implements
     IDataSinkSourceFactory<OrganisationElement.Builder, OrganisationElement, IBaseCollection<OrganisationElement.Builder, OrganisationElement, ?>> {
 
   @Override
-  public DataSinkSource<Builder, OrganisationElement, IBaseCollection<Builder, OrganisationElement, ?>> getDataSinkSourceFor(IBaseCollection<OrganisationElement.Builder, OrganisationElement, ?> validationObject) {
+  public DataSinkSource<OrganisationElement.Builder, OrganisationElement, IBaseCollection<OrganisationElement.Builder, OrganisationElement, ?>> getDataSinkSourceFor(IBaseCollection<OrganisationElement.Builder, OrganisationElement, ?> validationObject) {
     return new OrganisationDataSink(validationObject);
   }
 
