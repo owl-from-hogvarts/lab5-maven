7a4daae748dab93b7d429159160d9c64805716d7
owl-from-hogvarts <transftank@mail.ru>
2023-05-05 09:15:56 +0300

    chore: update example code

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/App.java
+++ organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/App.java
@@ -1,14 +1,12 @@
 package net.whitehorizont.apps.organization_collection_manager.core;
 
-import net.whitehorizont.apps.organization_collection_manager.core.collection.BaseId;
 import net.whitehorizont.apps.organization_collection_manager.core.collection.Collection;
 import net.whitehorizont.apps.organization_collection_manager.core.collection.CollectionManager;
-import net.whitehorizont.apps.organization_collection_manager.core.collection.CollectionMetadata;
 import net.whitehorizont.apps.organization_collection_manager.core.collection.OrganisationDataSinkSourceFactory;
 import net.whitehorizont.apps.organization_collection_manager.core.collection.OrganisationElement;
+import net.whitehorizont.apps.organization_collection_manager.core.collection.UUID_ElementId;
 import net.whitehorizont.apps.organization_collection_manager.core.collection.OrganisationElement.Builder;
 import net.whitehorizont.apps.organization_collection_manager.core.storage.FileStorage;
-import net.whitehorizont.apps.organization_collection_manager.core.storage.IBaseStorage;
 import net.whitehorizont.apps.organization_collection_manager.core.storage.collection_adapter.CollectionAdapter;
 import net.whitehorizont.apps.organization_collection_manager.core.storage.errors.StorageInaccessibleError;
 
@@ -16,9 +14,19 @@ public class App {
   public static void main(String[] args) throws StorageInaccessibleError {
     final var organisationDataSinkFactory = new OrganisationDataSinkSourceFactory();
     final var xmlCollectionAdapter = new CollectionAdapter<>(organisationDataSinkFactory);
-    final IBaseStorage<Collection<Builder, OrganisationElement>, BaseId, CollectionMetadata> testStorage = new FileStorage<>("./test.xml", xmlCollectionAdapter);
-    final CollectionManager<Collection<Builder, OrganisationElement>, IBaseStorage<Collection<Builder, OrganisationElement>, BaseId, CollectionMetadata>, BaseId> collectionManager = new CollectionManager<>();
+    final var testStorage = new FileStorage<>("./test.xml", xmlCollectionAdapter);
+    final CollectionManager<Collection<Builder, OrganisationElement>> collectionManager = new CollectionManager<>();
     collectionManager.addStorage(testStorage);
-    collectionManager.getCollection();
+   
+    final var defaultCollection$ = collectionManager.getCollection();
+    defaultCollection$.subscribe((collection) -> {
+      final var testElement = new OrganisationElement.Builder()
+          .name("White Horizont holding")
+          .ID(new UUID_ElementId());
+
+      collection.getDataSink().supply(testElement);
+      final var collectionId = collection.getMetadataSnapshot().getId();
+      collectionManager.save(collectionId);
+    });
   }
 }
