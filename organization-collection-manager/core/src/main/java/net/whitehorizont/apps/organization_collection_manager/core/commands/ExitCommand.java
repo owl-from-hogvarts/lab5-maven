672a611abb0e7682b49abb4509e60c0851c35f19
owl-from-hogvarts <transftank@mail.ru>
2023-05-12 20:39:28 +0300

    chore: remove stale references to old command hierarchy

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/commands/ExitCommand.java
+++ organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/commands/ExitCommand.java
@@ -1,30 +1,19 @@
 package net.whitehorizont.apps.organization_collection_manager.core.commands;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
-
 import io.reactivex.rxjava3.core.Observable;
-import net.whitehorizont.apps.organization_collection_manager.core.collection.ICollectionManager;
-import net.whitehorizont.libs.file_system.AssertHelpers;
 
 @NonNullByDefault
-public class ExitCommand implements ISystemCommand<Void> {
-  private @Nullable CommandQueue commandQueue;
+public class ExitCommand implements ICommand<Void> {
+  private CommandQueue commandQueue;
   // private @Nullable ICollectionManager<?, ?> collectionManager;
+
+  public ExitCommand(CommandQueue commandQueue) {
+    this.commandQueue = commandQueue;
+  }
 
   @Override
   public Observable<Void> execute() {
-    assert commandQueue != null : AssertHelpers.getAssertMessageFor("ExitCommand.java", "execute");
     return commandQueue.terminate();
-  }
-
-  @Override
-  public void setCommandQueue(CommandQueue queue) {
-    this.commandQueue = queue;
-  }
-
-  @Override
-  public void setCollectionManager(ICollectionManager<?, ?> collectionManager) {
-  }
-  
+  }  
 }
