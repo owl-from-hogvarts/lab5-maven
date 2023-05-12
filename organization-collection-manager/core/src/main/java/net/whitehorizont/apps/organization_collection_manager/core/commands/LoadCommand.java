fd0033a2a4ee3cb0b0a29d1b556dfcae9b2f667f
owl-from-hogvarts <transftank@mail.ru>
2023-05-12 20:40:45 +0300

    feat: implement load command

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/commands/LoadCommand.java
+++ organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/commands/LoadCommand.java
@@ -1,30 +1,27 @@
 package net.whitehorizont.apps.organization_collection_manager.core.commands;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import net.whitehorizont.apps.organization_collection_manager.core.collection.ICollection;
 import net.whitehorizont.apps.organization_collection_manager.core.collection.ICollectionElement;
 
+@NonNullByDefault
 public class LoadCommand<E extends ICollectionElement<?>> implements ICommand<E> {
+
+  private final CollectionCommandReceiver<?, E, ?> receiver;
+
+
+  public LoadCommand(CollectionCommandReceiver<?, E, ?> receiver) {
+    this.receiver = receiver;
+  }
+
 
   @Override
   public Observable<@NonNull E> execute() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'execute'");
-  }
+    return receiver.getEvery$();
 
-  @Override
-  public void setCollection(@NonNull Observable<@NonNull ICollection<?, ?, ?>> collection) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'setCollection'");
   }
-
-  @Override
-  public boolean hasPreferredCollection() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'hasPreferredCollection'");
-  }
-
   // create instance of FileStorage
   // place it into collection manager
   
