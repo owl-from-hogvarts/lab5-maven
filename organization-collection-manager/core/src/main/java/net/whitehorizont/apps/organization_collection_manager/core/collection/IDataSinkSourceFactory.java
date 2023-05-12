f2313f60bab8eaea9d425e8fa5e5e8307a61d72f
owl-from-hogvarts <transftank@mail.ru>
2023-05-12 11:31:52 +0300

    chore: get rid of IDataSinkFiles

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/collection/IDataSinkSourceFactory.java
+++
@@ -1,8 +0,0 @@
-package net.whitehorizont.apps.organization_collection_manager.core.collection;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
-@NonNullByDefault
-public interface IDataSinkSourceFactory<P extends IElementPrototype, E, V> {
-  DataSinkSource<P, E, V> getDataSinkSourceFor(V validationObject);
-}
