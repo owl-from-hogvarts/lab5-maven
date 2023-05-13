c1beac911200d676b8e725e786c43c1db0a31b7b
owl-from-hogvarts <transftank@mail.ru>
2023-05-13 14:39:07 +0300

    chore(cli): simplest error handling

--- organization-collection-manager/cli/src/main/java/net/whitehorizont/apps/organization_collection_manager/cli/CLI.java
+++ organization-collection-manager/cli/src/main/java/net/whitehorizont/apps/organization_collection_manager/cli/CLI.java
@@ -1,20 +1,19 @@
 package net.whitehorizont.apps.organization_collection_manager.cli;
 
+import java.io.PrintStream;
+
 import net.whitehorizont.apps.organization_collection_manager.cli.errors.IncorrectNumberOfArguments;
-import net.whitehorizont.apps.organization_collection_manager.core.commands.CollectionCommandReceiver;
 import net.whitehorizont.apps.organization_collection_manager.core.commands.CommandQueue;
 
 public class CLI {
   private final CommandQueue commandQueue;
   private final Greeter greeter;
-  private final CollectionCommandReceiver collectionReceiver;
+  private final PrintStream err;
 
   public CLI(Greeter greeter, CommandQueue commandQueue) {
     this.commandQueue = commandQueue;
     this.greeter = greeter;
-
-    
-
+    this.err = new PrintStream(greeter.getErrorStream());
     // listen on greeter for new cli commands
     // map them to actual commands
     // send actual commands to queue
@@ -22,13 +21,19 @@ public class CLI {
 
   public void start() throws IncorrectNumberOfArguments {
     while (true) {
-      final var commandMaybe = greeter.promptCommand();
-      if (commandMaybe.isEmpty()) {
+      try {
+
+        final var commandMaybe = greeter.promptCommand();
+        if (commandMaybe.isEmpty()) {
+          continue;
+        }
+
+        final var command = commandMaybe.get();
+        commandQueue.push(command).subscribe();
+      } catch (Exception e) {
+        err.println(e.getMessage());
         continue;
       }
-
-      final var command = commandMaybe.get();
-      commandQueue.push(command).subscribe();
     }
   }
 }
