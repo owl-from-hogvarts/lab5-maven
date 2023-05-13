f389581d152335a6bfe39263a7b59adc56cc762c
owl-from-hogvarts <transftank@mail.ru>
2023-05-13 09:00:13 +0300

    feat: initial `CLI` implementation

--- organization-collection-manager/cli/src/main/java/net/whitehorizont/apps/organization_collection_manager/cli/CLI.java
+++ organization-collection-manager/cli/src/main/java/net/whitehorizont/apps/organization_collection_manager/cli/CLI.java
@@ -1,13 +1,34 @@
 package net.whitehorizont.apps.organization_collection_manager.cli;
 
+import net.whitehorizont.apps.organization_collection_manager.cli.errors.IncorrectNumberOfArguments;
+import net.whitehorizont.apps.organization_collection_manager.core.commands.CollectionCommandReceiver;
+import net.whitehorizont.apps.organization_collection_manager.core.commands.CommandQueue;
+
 public class CLI {
-  public CLI() {
-    // greeter
-    // input, output, error streams
-    // command queue
+  private final CommandQueue commandQueue;
+  private final Greeter greeter;
+  private final CollectionCommandReceiver collectionReceiver;
+
+  public CLI(Greeter greeter, CommandQueue commandQueue) {
+    this.commandQueue = commandQueue;
+    this.greeter = greeter;
+
+    
 
     // listen on greeter for new cli commands
     // map them to actual commands
     // send actual commands to queue
+  }
+
+  public void start() throws IncorrectNumberOfArguments {
+    while (true) {
+      final var commandMaybe = greeter.promptCommand();
+      if (commandMaybe.isEmpty()) {
+        continue;
+      }
+
+      final var command = commandMaybe.get();
+      commandQueue.push(command).subscribe();
+    }
   }
 }
