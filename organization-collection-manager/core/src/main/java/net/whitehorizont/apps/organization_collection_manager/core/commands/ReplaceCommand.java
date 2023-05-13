fb7cbceb865cce9064acecd1cf47c35d07640db4
owl-from-hogvarts <transftank@mail.ru>
2023-05-13 09:02:05 +0300

    chore: delete stale unimplemented commands

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/commands/ReplaceCommand.java
+++
@@ -1,9 +0,0 @@
-package net.whitehorizont.apps.organization_collection_manager.core.commands;
-
-public class ReplaceCommand extends MetaCommand {
-  public ReplaceCommand() {
-    // create insert command instance
-    // create remove command instance
-    // add those command to internal queue (pass them to meta command constructor)
-  }
-}
