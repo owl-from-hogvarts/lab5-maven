2bcb2a7d4aa68f25fb743b8ebfcd3cbc4212927a
owl-from-hogvarts <transftank@mail.ru>
2023-05-13 09:03:33 +0300

    chore: update app

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/App.java
+++ organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/App.java
@@ -1,6 +1,5 @@
 package net.whitehorizont.apps.organization_collection_manager.core;
 
-
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
 import net.whitehorizont.apps.organization_collection_manager.core.collection.RamCollection;
@@ -13,7 +12,6 @@ import net.whitehorizont.apps.organization_collection_manager.core.collection.co
 import net.whitehorizont.apps.organization_collection_manager.core.collection.keys.UUID_ElementId;
 import net.whitehorizont.apps.organization_collection_manager.core.commands.CollectionCommandReceiver;
 import net.whitehorizont.apps.organization_collection_manager.core.commands.CommandQueue;
-import net.whitehorizont.apps.organization_collection_manager.core.commands.ICommand;
 import net.whitehorizont.apps.organization_collection_manager.core.commands.InsertCommand;
 import net.whitehorizont.apps.organization_collection_manager.core.storage.FileStorage;
 import net.whitehorizont.apps.organization_collection_manager.core.storage.collection_adapter.CollectionAdapter;
@@ -28,35 +26,41 @@ public class App {
     final ICollectionManager<RamCollection<Builder, OrganisationElement>, CollectionMetadata> collectionManager = new CollectionManager<>();
     testStorage.loadMetadata();
     collectionManager.addStorage(testStorage);
-    
+
     final var commandQueue = new CommandQueue();
 
     final var defaultCollection$ = collectionManager.getCollection();
     defaultCollection$.subscribe((collection) -> {
       // builder = collection.getDataSink().getBuilder()
       // for (final var field : builder.getFields())
+      // TODO: handle exception
       final var testElement = new OrganisationElement.Builder()
-          .name("Google")
+          .name("")
           .ID(new UUID_ElementId());
           
       final var collectionReceiver = new CollectionCommandReceiver<>(collection);
-      final ICommand<Void> testInsert = new InsertCommand<Builder>(testElement, collectionReceiver);
-      commandQueue.push(testInsert);
+      // final var testCommand = new
+      // LoadCommand<OrganisationElement>(collectionReceiver);
+      final var testCommand = new InsertCommand<>(testElement, collectionReceiver);
+      commandQueue.push(testCommand).subscribe(System.out::println);
+
+      // collectionManager.save(collection.getMetadataSnapshot().getId());
     });
-      // sometimes java can't infer types for us so we should help it to kill itself and write in some better language
+    // sometimes java can't infer types for us so we should help it to kill itself
+    // and write in some better language
 
     // get collection
     // get prototype for element
     // get prototype's fields (getFields)
     // for each field
-      // get metadata
-      // supply prompt with provided metadata and field's value primitive type
-      // prompt will ask user for input and transform value to desired type
-      // receive value from prompt
-      // call setValue for field with supplied value
+    // get metadata
+    // supply prompt with provided metadata and field's value primitive type
+    // prompt will ask user for input and transform value to desired type
+    // receive value from prompt
+    // call setValue for field with supplied value
     // when element is build
     // supply it to collection's dataSink
     // dataSink will construct actual element and request element validation
-    
+
   }
 }
