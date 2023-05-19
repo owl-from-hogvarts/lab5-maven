37a039456582fc64a87686050c54d43567958f8f
owl-from-hogvarts <transftank@mail.ru>
2023-05-19 22:45:42 +0300

    chore: nullness annotation

--- organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/StringFactory.java
+++ organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/StringFactory.java
@@ -1,5 +1,8 @@
 package net.whitehorizont.apps.organization_collection_manager.lib;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+@NonNullByDefault
 public class StringFactory implements IFromStringBuilder<String> {
 
   @Override
