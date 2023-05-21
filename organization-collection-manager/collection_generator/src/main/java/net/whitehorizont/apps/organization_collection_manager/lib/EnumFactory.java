6a21fdb35c4caa5e3aecf09af79c89932dc0a5c5
owl-from-hogvarts <transftank@mail.ru>
2023-05-21 17:42:07 +0300

    feat: error message for enum fields

--- organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/EnumFactory.java
+++ organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/EnumFactory.java
@@ -1,5 +1,8 @@
 package net.whitehorizont.apps.organization_collection_manager.lib;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EnumFactory<E extends Enum<E>> implements IFromStringBuilder<E> {
 
   private final Class<E> enumClass;
@@ -10,17 +13,22 @@ public class EnumFactory<E extends Enum<E>> implements IFromStringBuilder<E> {
 
   @Override
   public E buildFromString(String string) throws ValidationError {
-    return Enum.valueOf(enumClass, string);
+    try {
+      return Enum.valueOf(enumClass, string);
+    } catch (IllegalArgumentException e) {
+      throw new ValidationError(buildErrorMessage(string), e);
+    }
   }
-  
-}
 
+  private String buildErrorMessage(String input) {
+    final List<String> allowedValues = new ArrayList<>();
+    for (final var value : enumClass.getEnumConstants()) {
+      allowedValues.add(value.toString());
+    }
 
-enum Test {
-
-}
-
-class A {
-  {
+    return "input can be exactly one of following:\n" 
+      + String.join("\n", allowedValues) 
+      + "\nGot " + input;
   }
+
 }
