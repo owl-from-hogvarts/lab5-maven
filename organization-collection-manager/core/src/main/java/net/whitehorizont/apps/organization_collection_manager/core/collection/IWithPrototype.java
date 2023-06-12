d0663c34d2cfb0d7e29425037199a699c4f00b1d
owl-from-hogvarts <transftank@mail.ru>
2023-06-12 12:01:16 +0300

    chore: trash element factory and element prototype interface

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/collection/IWithPrototype.java
+++
@@ -1,7 +0,0 @@
-package net.whitehorizont.apps.organization_collection_manager.core.collection;
-
-import net.whitehorizont.apps.organization_collection_manager.lib.IReadonlyTreeProvider;
-
-public interface IWithPrototype<P extends IElementPrototype<?>> extends IReadonlyTreeProvider {
-  P getPrototype();
-}
