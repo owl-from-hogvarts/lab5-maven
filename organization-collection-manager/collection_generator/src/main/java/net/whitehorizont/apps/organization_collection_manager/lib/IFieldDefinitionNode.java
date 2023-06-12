e413702efe67f6668eac194cee0d8ba500150b0b
owl-from-hogvarts <transftank@mail.ru>
2023-06-12 11:59:51 +0300

    chore: get rid of redundant redundant interfaces

--- organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/IFieldDefinitionNode.java
+++
@@ -1,6 +0,0 @@
-package net.whitehorizont.apps.organization_collection_manager.lib;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
-@NonNullByDefault
-public class IFieldDefinitionNode extends Node<IFieldDefinitionNode, ValidatedFieldDefinition<?, ?>> {}
--- organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/IWriteableFieldDefinitionNode.java
+++
@@ -1,6 +0,0 @@
-package net.whitehorizont.apps.organization_collection_manager.lib;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
-@NonNullByDefault
-public class IWriteableFieldDefinitionNode extends Node<WritableFromStringFieldDefinition<?>> {}
