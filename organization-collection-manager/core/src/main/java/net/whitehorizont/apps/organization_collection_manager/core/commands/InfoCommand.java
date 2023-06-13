eedd5c9080f02b1926afcd7102e999c7cadafe35
owl-from-hogvarts <transftank@mail.ru>
2023-06-13 09:12:16 +0300

    fix(core): `info` command use new generics

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/commands/InfoCommand.java
+++ organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/commands/InfoCommand.java
@@ -7,16 +7,16 @@ import net.whitehorizont.apps.organization_collection_manager.lib.ReadonlyField;
 import net.whitehorizont.apps.organization_collection_manager.lib.TitledNode;
 
 @NonNullByDefault
-public class InfoCommand implements ICommand<TitledNode<ReadonlyField<?>>> {
+public class InfoCommand implements ICommand<TitledNode<?, ReadonlyField<?>>> {
 
-  private final CollectionCommandReceiver<?, ?> collection;
+  private final CollectionCommandReceiver<?> collection;
 
-  public InfoCommand(CollectionCommandReceiver<?, ?> collection) {
+  public InfoCommand(CollectionCommandReceiver<?> collection) {
     this.collection = collection;
   }
 
   @Override
-  public Observable<TitledNode<ReadonlyField<?>>> execute() {
+  public Observable<TitledNode<?, ReadonlyField<?>>> execute() {
     return Observable.just(collection.getMetadataTree());
   }
   
