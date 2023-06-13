80d025ddb71558619bd41fc8e13f60b8b93abec9
owl-from-hogvarts <transftank@mail.ru>
2023-06-13 14:27:19 +0300

    feat: make `Y` field for coordinates

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/collection/Coordinates.java
+++ organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/collection/Coordinates.java
@@ -21,20 +21,32 @@ public class Coordinates implements IWithPrototype<Coordinates.CoordinatesProtot
   
   private static final FieldMetadataWithValidators<Integer, Object> X_METADATA = new FieldMetadataWithValidators.Metadata<Integer, Object>().addValidator((value, _unused) -> {
     final int x = value.intValue();
-    final var isValueOk = x > -802;
+    final var isValueOk = x < 904;
     final var result = new ValidationResult<>(isValueOk, "Value should be strictly above -802");
     return result;
   }).setDisplayedName("X")
   .build();
 
+  private static final FieldMetadataWithValidators<Integer, Object> Y_METADATA = new FieldMetadataWithValidators.Metadata<Integer, Object>()
+    .setDisplayedName("Y")
+    .build();
+  
   private final FieldDefinition<Integer, ?> x;
+  private final FieldDefinition<Integer, ?> y;
 
   public Coordinates(CoordinatesPrototype prototype) throws ValidationError {
     this.x = new FieldDefinition<Integer, Object>(X_METADATA, prototype.x.getValue(), new Object());
+    this.y = new FieldDefinition<Integer, Object>(Y_METADATA, prototype.y.getValue(), new Object());
   }
 
   public static class CoordinatesRawData {
     private int x = 0;
+    private int y = 0;
+
+    public CoordinatesRawData setY(int y) {
+      this.y = y;
+      return this;
+    }
 
     public CoordinatesRawData setX(int x) {
       this.x = x;
@@ -44,10 +56,12 @@ public class Coordinates implements IWithPrototype<Coordinates.CoordinatesProtot
 
   public static class CoordinatesPrototype implements IElementPrototype<Coordinates.CoordinatesRawData> {
     private final WritableFromStringFieldDefinition<Integer> x;
+    private final WritableFromStringFieldDefinition<Integer> y;
 
     public CoordinatesPrototype() {
       try {
         this.x = new WritableFromStringFieldDefinition<Integer>(X_METADATA, 0, new IntegerFactory());
+        this.y = new WritableFromStringFieldDefinition<Integer>(Y_METADATA, 0, new IntegerFactory());
       } catch (ValidationError e) {
         assert false;
         throw new RuntimeException();
@@ -56,12 +70,13 @@ public class Coordinates implements IWithPrototype<Coordinates.CoordinatesProtot
 
     @Override
     public CoordinatesRawData getRawElementData() {
-      return new CoordinatesRawData().setX(this.x.getValue());
+      return new CoordinatesRawData().setX(this.x.getValue()).setY(this.y.getValue());
     }
 
     @Override
     public IElementPrototype<CoordinatesRawData> setFromRawData(CoordinatesRawData rawData) throws ValidationError {
       this.x.setValue(rawData.x);
+      this.y.setValue(rawData.y);
       return this;
     }
 
@@ -69,6 +84,7 @@ public class Coordinates implements IWithPrototype<Coordinates.CoordinatesProtot
     public Iterable<WritableFromStringFieldDefinition<?>> getWriteableFromStringFields() {
       final List<WritableFromStringFieldDefinition<?>> fields = new ArrayList<>();
       fields.add(x);
+      fields.add(y);
 
       return fields;
     }
@@ -94,6 +110,7 @@ public class Coordinates implements IWithPrototype<Coordinates.CoordinatesProtot
     final var prototype = new CoordinatesPrototype();
     try {
       prototype.x.setValue(this.x.getValue());
+      prototype.y.setValue(this.y.getValue());
 
       return prototype;
     } catch (ValidationError e) {
@@ -108,6 +125,7 @@ public class Coordinates implements IWithPrototype<Coordinates.CoordinatesProtot
   public TitledNode<ReadonlyField<?>> getTree() {
     final List<ReadonlyField<?>> leafs = new ArrayList<>();
     leafs.add(new ReadonlyField<>(X_METADATA, x.getValue()));
+    leafs.add(new ReadonlyField<>(Y_METADATA, y.getValue()));
 
     return new TitledNode<>(COORDINATES_TITLE, leafs, new ArrayList<>());
   }
