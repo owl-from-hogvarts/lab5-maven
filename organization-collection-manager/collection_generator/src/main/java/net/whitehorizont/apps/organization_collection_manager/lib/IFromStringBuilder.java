7861f7cb094f9d5fc9787edcf43568e794e7a34a
owl-from-hogvarts <transftank@mail.ru>
2023-06-16 11:33:54 +0300

    chore: nullness

--- organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/IFromStringBuilder.java
+++ organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/IFromStringBuilder.java
@@ -1,10 +1,12 @@
 package net.whitehorizont.apps.organization_collection_manager.lib;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
 import net.whitehorizont.apps.organization_collection_manager.lib.validators.ValidationError;
 
 @NonNullByDefault
 public interface IFromStringBuilder<V> {
   /** NullPointerException if any is intercepted and interpreted as null value */
-  V buildFromString(String string) throws ValidationError;
+  V buildFromString(@Nullable String string) throws ValidationError;
 }
