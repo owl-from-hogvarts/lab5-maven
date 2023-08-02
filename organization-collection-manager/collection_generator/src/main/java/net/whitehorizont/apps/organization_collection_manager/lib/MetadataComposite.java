b2cc1034dbeca4fc9da9ce2cd94b91d9d6dbd09b
owl-from-hogvarts <transftank@mail.ru>
2023-08-02 16:39:24 +0300

    feat: implement fill and extended validate operations

--- organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/MetadataComposite.java
+++ organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/MetadataComposite.java
@@ -9,9 +9,15 @@ import net.whitehorizont.apps.organization_collection_manager.lib.FieldMetadataE
 import net.whitehorizont.apps.organization_collection_manager.lib.validators.ValidationError;
 
 
+/** 
+ * @param <ParentHost> parent context object
+ * @param <Host> current context object
+ * @param <WritableHost> 
+ * @param <T> Validation object
+ */
 @NonNullByDefault
-// we know type of node container i.e. TitledNode but now what is inside the container
-public class MetadataComposite<ParentHost, Host, WritableHost extends Host, T> extends TitledNode<MetadataComposite<Host, ?, ?, ? super T>, FieldMetadataExtended<Host, WritableHost, ?>> implements ICanValidate<Host, T> {
+// we know type of node container i.e. TitledNode but now what is inside of it
+public class MetadataComposite<ParentHost, Host, WritableHost extends Host, T > extends TitledNode<MetadataComposite<Host, ?, ?, ? super T>, FieldMetadataExtended<Host, WritableHost, ?>> implements IElementInfoProvider<Host, T>, ICanFill<Host, WritableHost> {
   private final Function<ParentHost, WritableHost> hostExtractor;
 
   public MetadataComposite(String displayedName, List<FieldMetadataExtended<Host, WritableHost, ?>> leafs,
@@ -23,7 +29,15 @@ public class MetadataComposite<ParentHost, Host, WritableHost extends Host, T> e
   @Override
   public void validate(Host host, T validationObject) throws ValidationError {
     for (final var leaf : getLeafs()) {
-      leaf.validate(host, validationObject);
+      leaf.validate(host);
+      
+      // tried to overcome this crutch by using visitor pattern
+      // turns out it is impossible to apply visitor pattern here
+      // because there is a need to unwrap context object in type safe way
+      // which is too hard or even impossible in java
+      if (leaf instanceof FieldMetadataExtendedWithRichValidators) {
+        ((FieldMetadataExtendedWithRichValidators<Host, WritableHost, ?, T>) leaf).validate(host, validationObject);
+      }
     }
 
     for (final var child : getChildren()) {
@@ -44,9 +58,17 @@ public class MetadataComposite<ParentHost, Host, WritableHost extends Host, T> e
     }
 
     for (final var child : getChildren()) {
-      child.fill(to, from);
+      fillChild(child, to, from);
     }
   }
+
+  private static <ParentHost, Host, ChildHost extends Host> void fillChild(MetadataComposite<ParentHost, Host, ChildHost, ?> metadata, ParentHost to, ParentHost from) {
+    final var toChild = metadata.extractChildHost(to);
+    final var fromChild = metadata.extractChildHost(from);
+
+    metadata.fill(toChild, fromChild);
+
+  } 
 
   public void fill(WritableHost to, Host from, Tag tag) {
     for (final var leaf : getLeafs()) {
@@ -54,9 +76,17 @@ public class MetadataComposite<ParentHost, Host, WritableHost extends Host, T> e
     }
 
     for (final var child : getChildren()) {
-      child.fill(to, from, tag);
+      fillChild(child, to, from, tag);
     }
   }
+
+  private static <ParentHost, Host, ChildHost extends Host> void fillChild(MetadataComposite<ParentHost, Host, ChildHost, ?> metadata, ParentHost to, ParentHost from, Tag tag) {
+    final var toChild = metadata.extractChildHost(to);
+    final var fromChild = metadata.extractChildHost(from);
+
+    metadata.fill(toChild, fromChild, tag);
+
+  } 
 
   public WritableHost extractChildHost(ParentHost parentHost) {
     return hostExtractor.apply(parentHost);
