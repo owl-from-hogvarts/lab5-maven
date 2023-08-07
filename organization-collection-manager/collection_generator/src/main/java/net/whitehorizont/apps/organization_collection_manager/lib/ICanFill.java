7e00be85b6522ef177fba849c49867cccd82d94d
owl-from-hogvarts <transftank@mail.ru>
2023-08-07 15:58:30 +0300

    chore: nullness

--- organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/ICanFill.java
+++ organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/ICanFill.java
@@ -1,8 +1,11 @@
 package net.whitehorizont.apps.organization_collection_manager.lib;
 
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
 import net.whitehorizont.apps.organization_collection_manager.lib.FieldMetadataExtended.Tag;
 
+@NonNullByDefault
 public interface ICanFill<Host, WritableHost extends Host> {
   /** 
    * Updates <i>all</i> fields of {@code base} object with values from other object 
