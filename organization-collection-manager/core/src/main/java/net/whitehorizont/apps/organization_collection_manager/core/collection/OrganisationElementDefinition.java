93add933017ffb3c57e55d08cf8d2385656361e8
owl-from-hogvarts <transftank@mail.ru>
2023-08-09 16:12:47 +0300

    chore: format

--- organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/collection/OrganisationElementDefinition.java
+++ organization-collection-manager/core/src/main/java/net/whitehorizont/apps/organization_collection_manager/core/collection/OrganisationElementDefinition.java
@@ -22,28 +22,29 @@ public class OrganisationElementDefinition {
   static final String ELEMENT_TITLE = "Organisation";
 
   // !!! METADATA !!!
-  public static final FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, String> NAME_METADATA = FieldMetadataExtended.<OrganisationElement, OrganisationElementWritable, String>builder()
-          .setDisplayedName("name")
-          .setRequired("Company name must be specified")
-          .setValueBuilder(new StringFactory())
-          .setValueSetter((element, name) -> element.name(name))
-          .setValueGetter(element -> element.getName())
-          .addSimpleValidator((value) -> new ValidationResult<>(value.length() >= 1, "String should not be empty"))
-          .build();
+  public static final FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, String> NAME_METADATA = FieldMetadataExtended
+      .<OrganisationElement, OrganisationElementWritable, String>builder()
+      .setDisplayedName("name")
+      .setRequired("Company name must be specified")
+      .setValueBuilder(new StringFactory())
+      .setValueSetter((element, name) -> element.name(name))
+      .setValueGetter(element -> element.getName())
+      .addSimpleValidator((value) -> new ValidationResult<>(value.length() >= 1, "String should not be empty"))
+      .build();
 
-  public static final FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, UUID_ElementId> ID_METADATA = 
-      FieldMetadataExtended.<OrganisationElement, OrganisationElementWritable, UUID_ElementId>builder()
-          .setDisplayedName("ID")
-          .setRequired("ID must be provided for collection element")
-          .setValueSetter((element, ID) -> element.ID(ID))
-          .setValueGetter(element -> element.getID())
-          .addTag(Tag.PRESERVE)
-          .addTag(Tag.SKIP_INTERACTIVE_INPUT)
-          .setValueBuilder((idString) -> new UUID_ElementId(idString))
-          .build();
+  public static final FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, UUID_ElementId> ID_METADATA = FieldMetadataExtended
+      .<OrganisationElement, OrganisationElementWritable, UUID_ElementId>builder()
+      .setDisplayedName("ID")
+      .setRequired("ID must be provided for collection element")
+      .setValueSetter((element, ID) -> element.ID(ID))
+      .setValueGetter(element -> element.getID())
+      .addTag(Tag.PRESERVE)
+      .addTag(Tag.SKIP_INTERACTIVE_INPUT)
+      .setValueBuilder((idString) -> new UUID_ElementId(idString))
+      .build();
 
-  public static final FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, OrganisationType> TYPE_METADATA =
-      FieldMetadataExtended.<OrganisationElement, OrganisationElementWritable, OrganisationType>builder()
+  public static final FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, OrganisationType> TYPE_METADATA = FieldMetadataExtended
+      .<OrganisationElement, OrganisationElementWritable, OrganisationType>builder()
       .setDisplayedName("type")
       .setValueBuilder(new EnumFactory<>(OrganisationType.class))
       .setRequired("Type of organisation should be specified!")
@@ -52,16 +53,15 @@ public class OrganisationElementDefinition {
       .setValueSetter((element, type) -> element.type(type))
       .build();
 
-  public static final FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, Double> ANNUAL_TURNOVER_METADATA =
-   FieldMetadataExtended.<OrganisationElement, OrganisationElementWritable, Double>builder()
-   .setDisplayedName("Annual Turnover")
-   .setRequired("Annual Turnover must be provided")
-   .addSimpleValidator(value -> new ValidationResult<>(value > 0.0, "Annual Turnover should be strictly above zero"))
-   .setValueGetter(element -> element.getAnnualTurnover())
-   .setValueSetter((element, annualTurnover) -> element.annualTurnover(annualTurnover))
-   .setValueBuilder(new DoubleFactory())
-   .build();
-  
+  public static final FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, Double> ANNUAL_TURNOVER_METADATA = FieldMetadataExtended
+      .<OrganisationElement, OrganisationElementWritable, Double>builder()
+      .setDisplayedName("Annual Turnover")
+      .setRequired("Annual Turnover must be provided")
+      .addSimpleValidator(value -> new ValidationResult<>(value > 0.0, "Annual Turnover should be strictly above zero"))
+      .setValueGetter(element -> element.getAnnualTurnover())
+      .setValueSetter((element, annualTurnover) -> element.annualTurnover(annualTurnover))
+      .setValueBuilder(new DoubleFactory())
+      .build();
 
   public static MetadataComposite<?, OrganisationElement, OrganisationElementWritable> getMetadata() {
     final List<FieldMetadataExtended<OrganisationElement, OrganisationElementWritable, ?>> leafs = new ArrayList<>();
@@ -73,7 +73,8 @@ public class OrganisationElementDefinition {
     final List<MetadataComposite<OrganisationElement, ?, ?>> children = new ArrayList<>();
     children.add(CoordinatesDefinition.<OrganisationElement>getTree((organisation) -> organisation.coordinates));
 
-    return new MetadataComposite<Object, OrganisationElement, OrganisationElementWritable>(ELEMENT_TITLE, leafs, children, null);
+    return new MetadataComposite<Object, OrganisationElement, OrganisationElementWritable>(ELEMENT_TITLE, leafs,
+        children, null);
   }
 
   public static class OrganisationElementFactory implements IWritableHostFactory<OrganisationElementWritable> {
@@ -84,33 +85,39 @@ public class OrganisationElementDefinition {
     }
 
   }
-  
+
   public static class OrganisationElement implements ICollectionElement<OrganisationElement> {
     protected String name;
     protected UUID_ElementId ID = new UUID_ElementId(); // init with default value which is easily overridable
     protected CoordinatesWriteable coordinates = new CoordinatesWriteable();
     protected OrganisationType type;
     protected Double annualTurnover;
+
     protected String getName() {
       return name;
     }
+
     protected UUID_ElementId getID() {
       return ID;
     }
+
     protected Coordinates getCoordinates() {
       return coordinates;
     }
+
     protected OrganisationType getType() {
       return type;
     }
+
     protected Double getAnnualTurnover() {
       return annualTurnover;
     }
+
     @Override
     public int compareTo(OrganisationElement other) {
-    // will not return null, 'cause field is not nullable
+      // will not return null, 'cause field is not nullable
       return (int) (this.annualTurnover - other.annualTurnover);
-  }
+    }
   }
 
   public static class OrganisationElementWritable extends OrganisationElement {
