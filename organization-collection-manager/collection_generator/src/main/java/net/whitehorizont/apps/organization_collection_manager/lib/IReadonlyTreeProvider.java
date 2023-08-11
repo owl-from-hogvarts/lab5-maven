a634948aad73341057ecdfbab9c8d1b7b0138c20
owl-from-hogvarts <transftank@mail.ru>
2023-08-11 14:11:47 +0300

    chore: remove unused classes

--- organization-collection-manager/collection_generator/src/main/java/net/whitehorizont/apps/organization_collection_manager/lib/IReadonlyTreeProvider.java
+++
@@ -1,8 +0,0 @@
-package net.whitehorizont.apps.organization_collection_manager.lib;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
-@NonNullByDefault
-public interface IReadonlyTreeProvider extends IDisplayable {
-  TitledNode<?, ReadonlyField<?>> getTree();
-}
