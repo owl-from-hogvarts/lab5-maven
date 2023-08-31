69747eb0671987f630a35a9e19e6c7d3cadc129f
BIZHAN28 <71879088+BIZHAN28@users.noreply.github.com>
2023-08-31 18:47:42 +0300

    feat: add DatagramSocketAdapter

---
+++ organization-collection-manager/network/src/main/java/net/whitehorizont/libs/network/transport/udp/DatagramSocketAdapter.java
@@ -0,0 +1,50 @@
+package net.whitehorizont.libs.network.transport.udp;
+
+import net.whitehorizont.libs.network.past.ITransport;
+import net.whitehorizont.libs.network.past.TransportPacket;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.util.Arrays;
+
+public class DatagramSocketAdapter implements ITransport<InetSocketAddress> {
+    private DatagramSocket datagramSocket;
+
+    public DatagramSocketAdapter(DatagramSocket datagramSocket) {
+        this.datagramSocket = datagramSocket;
+    }
+
+    @Override
+    public short getPacketLengthLimit() throws SocketException {
+        return (short) datagramSocket.getSendBufferSize();
+    }
+
+    @Override
+    public void send(byte[] packet, InetSocketAddress endpoint) {
+        try {
+            DatagramPacket datagramPacket = new DatagramPacket(packet, packet.length, endpoint);
+            datagramSocket.send(datagramPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public TransportPacket<InetSocketAddress> receive() {
+        try {
+            byte[] buffer = new byte[getPacketLengthLimit()];
+            DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+            datagramSocket.receive(datagramPacket);
+            InetSocketAddress senderAddress = (InetSocketAddress) datagramPacket.getSocketAddress();
+            byte[] data = Arrays.copyOf(datagramPacket.getData(), datagramPacket.getLength());
+            return new TransportPacket<>(senderAddress, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
